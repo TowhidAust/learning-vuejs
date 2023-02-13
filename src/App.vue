798ca2eaf3a12d6fd9b5e5219632323aee7f4965
TowhidAust <towhidaustcse33@gmail.com>
2023-02-13 14:14:33 +0600

    vue js getting started

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <TodoList/>
+  </div>
+</template>
+
+<script>
+  import TodoList from "./features/TodoList.vue"
+  export default {
+    name: "App",
+    components: {
+      TodoList
+    }
+  }
+</script>
+
+<style>
+    
+</style>
